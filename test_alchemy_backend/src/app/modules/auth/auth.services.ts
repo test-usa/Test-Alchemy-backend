be28bb52b377e07dee82976305fa4af1d5146fbe
habibimamrifat <habibrifatx21@gmail.com>
2025-01-21 17:58:12 +0600

    working  on jwt

--- test_alchemy_backend/src/app/modules/auth/auth.services.ts
+++ test_alchemy_backend/src/app/modules/auth/auth.services.ts
@@ -1,5 +1,21 @@
+import { UserMOdel } from "../user/user.model"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 const logIn = async (email: string, password: string) => {
-//    const result = await 
+    const findUserWithEmail = await UserMOdel.findOne({ email: email })
+    if (!findUserWithEmail) {
+        throw Error("no; user found with this email")
+    }
+    const match = await bcrypt.compare(password, findUserWithEmail.password);
+    if(!match)
+    {
+        throw   Error("password is not matched")
+    }
+
+    jwt.sign({
+        data: 'foobar'
+      }, 'secret', { expiresIn: '1h' });
 }
 
 const authSercvices = {
