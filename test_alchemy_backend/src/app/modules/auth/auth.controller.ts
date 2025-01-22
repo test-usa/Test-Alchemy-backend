53d0fa2ad201e7d2719e7adeea32f971fb4dbc0e
habibimamrifat <habibrifatx21@gmail.com>
2025-01-22 11:02:01 +0600

    working  on jwt log in

--- test_alchemy_backend/src/app/modules/auth/auth.controller.ts
+++ test_alchemy_backend/src/app/modules/auth/auth.controller.ts
@@ -4,8 +4,12 @@ import authSercvices from "./auth.services";
 const logIn = catchAsync(async(req, res) => {
     const {email,password}=req.body
     const result = await authSercvices.logIn(email,password)
+   const{approvalToken, refreshToken, findUserWithEmail}= result
     res.status(200).json({
-        message:"success"
+        message:"Log In Successful",
+        approvalToken:approvalToken,
+        refreshToken:refreshToken,
+        user:findUserWithEmail
     })
 })
 
--- test_alchemy_backend/src/app/modules/auth/auth.services.ts
+++ test_alchemy_backend/src/app/modules/auth/auth.services.ts
@@ -1,6 +1,8 @@
 import { UserMOdel } from "../user/user.model"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import createToken from "./auth.utill"
+import config from "../../config"
 
 const logIn = async (email: string, password: string) => {
     const findUserWithEmail = await UserMOdel.findOne({ email: email })
@@ -8,14 +10,22 @@ const logIn = async (email: string, password: string) => {
         throw Error("no; user found with this email")
     }
     const match = await bcrypt.compare(password, findUserWithEmail.password);
-    if(!match)
-    {
-        throw   Error("password is not matched")
+    if (!match) {
+        throw Error("password is not matched")
     }
 
-    jwt.sign({
-        data: 'foobar'
-      }, 'secret', { expiresIn: '1h' });
+  
+
+    // Tokenize user data
+    const tokenizeData = { id: findUserWithEmail.id, role: findUserWithEmail.userType };
+    const approvalToken = createToken(tokenizeData, config.jwt_token_secret as string, config.token_expairsIn as string);
+    const refreshToken = createToken(tokenizeData, config.jwt_refresh_Token_secret as string, config.rifresh_expairsIn as string);
+    
+    // console.log(approvalToken, refreshToken, findUserWithEmail)
+
+    return { approvalToken, refreshToken, findUserWithEmail };
+
+
 }
 
 const authSercvices = {
