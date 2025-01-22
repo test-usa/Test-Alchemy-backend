af4db6db20c4814cebc779949ae078411d233263
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-01-22 10:57:40 +0600

    Add: user module

--- test_alchemy_backend/src/app/modules/user/user.route.ts
+++ test_alchemy_backend/src/app/modules/user/user.route.ts
@@ -6,13 +6,18 @@ import {
   getSingleUser,
   updateUser,
 } from "./user.controller";
+import validator from "../../util/validator";
+import {
+  userUpdateValidationSchema,
+  userValidationSchema,
+} from "./user.validation";
 
 const router = express.Router();
 
 router.get("/", getAllUser);
 router.get("/:id", getSingleUser);
-router.post("/", createUser);
-router.patch("/", updateUser);
+router.post("/", validator(userValidationSchema), createUser);
+router.patch("/:id", validator(userUpdateValidationSchema), updateUser);
 router.delete("/:id", deleteUser);
 
 const userRoutes = router;
