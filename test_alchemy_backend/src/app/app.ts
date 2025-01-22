198017373189c0122ee9604ecd8b3c90e75edd08
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-01-22 10:59:18 +0600

    test

--- test_alchemy_backend/src/app/app.ts
+++ test_alchemy_backend/src/app/app.ts
@@ -1,27 +1,28 @@
-import express from "express"
-const app = express()
-import cors from "cors"
+import express from "express";
+const app = express();
+import cors from "cors";
 
-import globalErrorHandler from "./middlewares/globalerrorHandeller"
-import routeNotFound from "./middlewares/routNotFound"
-import Routes from "./routes"
+import globalErrorHandler from "./middlewares/globalerrorHandeller";
+import routeNotFound from "./middlewares/routNotFound";
+import Routes from "./routes";
 
 // middleWares
 app.use(express.json());
 app.use(cors());
 
 // Routs
-app.use("/api/v1",Routes)
+app.use("/api/v1", Routes);
 
 // route not found
-app.use(routeNotFound)
+app.use(routeNotFound);
 
 // global error handeller
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// comment
 
 export default app;
