516203a4bee1fbd62acede89acaa7e6591cfe907
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-01-25 12:05:41 +0600

    update: question paper

--- test_alchemy_backend/src/app/modules/questionPaper/questionPaper.validation.ts
+++ test_alchemy_backend/src/app/modules/questionPaper/questionPaper.validation.ts
@@ -8,23 +8,23 @@ const TMCQSchema = z.object({
   question: z.string(),
   options: z
     .array(z.string())
-    .nonempty()
+    .nonempty({ message: "MCQ options must not be empty" }) // Added message for empty options
     .min(2, { message: "MCQ options must have at least two options" }),
-  correctAns: z.enum([0, 1, 2, 3]),
+  correctAns: z.number().refine((val) => [0, 1, 2, 3].includes(val)),
   mark: z.number().min(1, "Mark must be greater than 0"),
 });
 
 // Question Paper Schema
 export const TQuestionPaperSchema = z.object({
-  id: z.string(),
-  domain: z.string(),
-  examineeId: z.string().refine((val) => Types.ObjectId.isValid(val), {
-    message: "Invalid ObjectId format",
+  body: z.object({
+    id: z.string(),
+    domain: z.string().nonempty({ message: "Domain must not be empty" }), // Added validation for non-empty domain
+    examineeId: z.string().optional(),
+    duration: z.number().min(1, "Duration must be a positive number"),
+    totalMarks: z.number().optional(),
+    MCQSet: z
+      .array(TMCQSchema) // Ensure this is an array of TMCQSchema objects
+      .nonempty({ message: "Question paper must have at least one question" }),
+    isDeleted: z.boolean().default(false),
   }),
-  duration: z.number().min(1, "Duration must be a positive number"),
-  totalMarks: z.number().optional(),
-  MCQSet: z
-    .array(TMCQSchema) // Ensure this is an array of TMCQSchema objects
-    .nonempty({ message: "Question paper must have at least one question" }),
-  isDeleted: z.boolean(),
 });
